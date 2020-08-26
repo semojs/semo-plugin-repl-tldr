35e07d0cc55e72b215ef4d50cee87bc5d1a79a2d
Richard Yu <vipzhicheng@gmail.com>
2020-08-26 20:13:11 +0800

    feat: use core console reader

--- src/common/tldr.ts
+++ src/common/tldr.ts
@@ -1,3 +1,5 @@
+import { Utils } from '@semo/core'
+
 import got from 'got'
 import marked from 'marked'
 import TerminalRenderer from 'marked-terminal'
@@ -5,13 +7,19 @@ marked.setOptions({
   renderer: new TerminalRenderer()
 })
 
+
+
 export const getTldr = async (input, opts) => {
   try {
     const type = opts.type || 'common'
     const langPage = opts.lang ? `pages.${opts.lang}` : 'pages'
     const url = `https://gitee.com/vipzhicheng/tldr/raw/master/${langPage}/${type}/${input}.md`
     const response: any = await got(url)
-    console.log(marked(response.body))
+
+    Utils.consoleReader(marked(response.body), {
+      plugin: 'semo-plugin-repl-tldr',
+      identifier: input
+    })
 
   } catch (e) {
     console.error(e.message)
