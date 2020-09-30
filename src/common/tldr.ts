f11daadeb4218522ccfbcbd7b911f9e75fa04d2b
Richard Yu <vipzhicheng@gmail.com>
2020-09-30 18:56:25 +0800

    adjust got headers

--- src/common/tldr.ts
+++ src/common/tldr.ts
@@ -11,8 +11,8 @@ marked.setOptions({
 
 export const getTldr = async (input, opts) => {
   try {
-    const type = 'common'
-    const langPage = 'pages'
+    const type = opts.type || 'common'
+    const langPage = opts.lang ? `pages.${opts.lang}` : 'pages'
     const url = `https://gitee.com/vipzhicheng/tldr/raw/master/${langPage}/${type}/${input}.md`
     const response: any = await got(url, {
       headers: {
